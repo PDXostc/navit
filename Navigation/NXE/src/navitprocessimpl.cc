22d56133de995d59abbc27a0ef9c9b894b30fb11
Bartosz Taczała <bartosz.taczala@mobica.com>
2015-02-05 14:22:25 +0100

    NXE: Boost-process implementation instead of QProcess

--- Navigation/NXE/src/navitprocessimpl.cc
+++ Navigation/NXE/src/navitprocessimpl.cc
@@ -1,15 +1,27 @@
 #include "navitprocessimpl.h"
 
-#include <QtCore/QProcess>
+#include <boost/process.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/system/error_code.hpp>
 
+#include <iostream>
+
+namespace bp = ::boost::process;
+namespace bs = ::boost::system;
+namespace bf = ::boost::filesystem;
+
+namespace {
+constexpr pid_t invalidPid = -1;
+}
 namespace NXE {
 
 struct NavitProcessImplPrivate {
-    const std::string m_navitProgramName = "navit";
-    std::string m_programPath;
 
-    std::list<std::string> m_args;
-    QProcess m_process;
+    const std::string m_navitProgramName = "navit";
+    std::string m_programPath            = "";
+    std::list<std::string> m_args        = {};
+    bp::child m_child                    = bp::child(invalidPid);
+    bs::error_code m_lastError           = bs::error_code(2,bs::generic_category());
 };
 
 
@@ -20,6 +32,10 @@ NavitProcessImpl::NavitProcessImpl():
 
 NavitProcessImpl::~NavitProcessImpl()
 {
+    if (d_ptr->m_child.pid != invalidPid) {
+        // ask via DBus to close itself
+        bp::terminate(d_ptr->m_child);
+    }
 }
 
 void NavitProcessImpl::setProgramPath(const std::string &name)
@@ -29,9 +45,14 @@ void NavitProcessImpl::setProgramPath(const std::string &name)
 
 void NavitProcessImpl::start()
 {
+    d_ptr->m_lastError = bs::error_code();
     const std::string command =  d_ptr->m_programPath + "/" + d_ptr->m_navitProgramName;
-    d_ptr->m_process.setProgram(command.c_str());
-    d_ptr->m_process.start();
+    bf::path exe = command;
+
+    d_ptr->m_child = bp::execute(bp::initializers::run_exe(exe),
+                                 bp::initializers::start_in_dir(d_ptr->m_programPath),
+                                 bp::initializers::inherit_env(),
+                                 bp::initializers::set_on_error(d_ptr->m_lastError));
 }
 
 void NavitProcessImpl::stop()
@@ -40,7 +61,7 @@ void NavitProcessImpl::stop()
 
 bool NavitProcessImpl::isRunning()
 {
-    return true;
+    return !(d_ptr->m_lastError);
 }
 
 void NavitProcessImpl::setArgs(const std::list<std::string> &args)
